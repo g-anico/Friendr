b98c83d7ab4353508e4fb2765b19f5902d0e01f2
Anco Chien <anco.chien@gmail.com>
2017-08-06 21:31:04 -0700

    friends array

---
+++ app/data/friends.js
@@ -0,0 +1,158 @@
+var tableArray = [
+
+{
+	name: "Big Bird",
+	photo: "https://vignette1.wikia.nocookie.net/muppet/images/9/92/Bigbirdnewversion.png/revision/latest?cb=20120128201030",
+	scores: [
+	2,
+  4,
+  1,
+  2,
+  4,
+  5,
+  1,
+  4,
+  5,
+  5
+
+	]
+},{
+	name: "Cookie Monster",
+	photo: "https://vignette2.wikia.nocookie.net/muppet/images/0/08/CookieMonsterWaving.jpg/revision/latest?cb=20120128192952",
+	scores: [
+	2,
+	2,
+	1,
+	3,
+	5,
+	3,
+	2,
+	1,
+	4,
+	4
+	]
+},{
+	name: "Elmo",
+	photo: "https://upload.wikimedia.org/wikipedia/en/7/74/Elmo_from_Sesame_Street.gif",
+	scores: [
+	2,
+	2,
+	5,
+	1,
+	3,
+	4,
+	4,
+	1,
+	5,
+	4,
+	]
+},{
+	name: "Ernie",
+	photo: "https://vignette1.wikia.nocookie.net/muppet/images/2/2e/ErnieFullFigure-NEW.jpg/revision/latest?cb=20100901224144",
+	scores: [
+	5,
+	2,
+	1,
+	1,
+	4,
+	5,
+	2,
+	1,
+	2,
+	3
+	]
+}, {
+	name: "Oscar the Grouch",
+	photo: "https://vignette3.wikia.nocookie.net/muppet/images/c/c3/Oscar-can2.jpg/revision/latest?cb=20090819003837",
+	scores: [
+	1,
+	5,
+	2,
+	1,
+	2,
+	2,
+	5,
+	1,
+	3,
+	5
+	]
+}, {
+	name: "Count von Count",
+	photo: "https://vignette2.wikia.nocookie.net/muppet/images/2/22/CountVonCount.png/revision/latest?cb=20150116064552",
+	scores: [
+	5,
+	3,
+	4,
+	5,
+	4,
+	2,
+	1,
+	4,
+	1,
+	1
+	]
+}, {
+	name: "Kermit the Frog",
+	photo: "https://upload.wikimedia.org/wikipedia/en/6/62/Kermit_the_Frog.jpg",
+	scores: [
+	2,
+	2,
+	1,
+	4,
+	4,
+	1,
+	5,
+	1,
+	2,
+	1
+	]
+}, {
+	name: "Hoots the Owl",
+	photo: "https://vignette2.wikia.nocookie.net/muppet/images/8/81/HootsTheOwl.png/revision/latest?cb=20110404163733",
+	scores: [
+	5,
+	1,
+	1,
+	2,
+	3,
+	4,
+	4,
+	4,
+	5,
+	1
+	]
+}, {
+	name: "Grover",
+	photo: "https://upload.wikimedia.org/wikipedia/en/b/be/Grover.JPG",
+	scores: [
+	2,
+	4,
+	4,
+	4,
+	5,
+	1,
+	3,
+	2,
+	1,
+	4
+	]
+}, {
+	name: "Snuffleupagus",
+	photo: "https://pbs.twimg.com/profile_images/669178197099986945/sWPLlMP3.jpg",
+	scores: [
+	1,
+	4,
+	5,
+	2,
+	4,
+	1,
+	5,
+	3,
+	1,
+	3
+	]
+},
+
+];
+//Lets us use this module in other .js files
+module.exports = tableArray;
